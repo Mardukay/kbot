0dd183015bc6ac03200c1e80c781018ade8e447a
Mardukay <110109686+Mardukay@users.noreply.github.com>
2024-01-21 04:04:48 +0200

    update

--- cmd/kbot.go
+++ cmd/kbot.go
@@ -114,7 +114,7 @@ to quickly create a Cobra application.`,
 
 func init() {
 	ctx := context.Background()
-	initMetrics(ctx
+	initMetrics(ctx)
 	rootCmd.AddCommand(kbotCmd)
 
 	// Here you will define your flags and configuration settings.
