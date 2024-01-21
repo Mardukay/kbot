b848c37e72a3a7c346626cec6352dab32325bde7
Mardukay <110109686+Mardukay@users.noreply.github.com>
2024-01-21 04:16:34 +0200

    update

--- cmd/kbot.go
+++ cmd/kbot.go
@@ -1,26 +1,28 @@
 /*
-Copyright © 2023 NAME HERE <EMAIL ADDRESS>
+Copyright © 2023 NAME HERE den.vasyliev@gmail.com
 */
 package cmd
 
 import (
+	"context"
 	"fmt"
-	"github.com/spf13/cobra"
-	telebot "gopkg.in/telebot.v3"
-	"log"
 	"os"
 	"time"
+
+	"github.com/spf13/cobra"
+
 	"github.com/hirosassa/zerodriver"
 	"go.opentelemetry.io/otel"
 	"go.opentelemetry.io/otel/exporters/otlp/otlpmetric/otlpmetricgrpc"
 	sdkmetric "go.opentelemetry.io/otel/sdk/metric"
 	"go.opentelemetry.io/otel/sdk/resource"
 	semconv "go.opentelemetry.io/otel/semconv/v1.12.0"
+	telebot "gopkg.in/telebot.v3"
 )
 
 var (
-	//Get token from env
-	token = os.Getenv("TELE_TOKEN")
+	// TeleToken bot
+	TeleToken   = os.Getenv("TELE_TOKEN")
 	// MetricsHost exporter host:port
 	MetricsHost = os.Getenv("METRICS_HOST")
 )
@@ -58,15 +60,14 @@ func initMetrics(ctx context.Context) {
 
 func pmetrics(ctx context.Context, payload string) {
 	// Get the global MeterProvider and create a new Meter with the name "kbot_light_signal_counter"
-	meter := otel.GetMeterProvider().Meter("kbot_message_counter")
+	meter := otel.GetMeterProvider().Meter("kbot_light_signal_counter")
 
 	// Get or create an Int64Counter instrument with the name "kbot_light_signal_<payload>"
-	counter, _ := meter.Int64Counter(fmt.Sprintf("kbot_message_%s", payload))
+	counter, _ := meter.Int64Counter(fmt.Sprintf("kbot_light_signal_%s", payload))
 
 	// Add a value of 1 to the Int64Counter
 	counter.Add(ctx, 1)
 }
-
 
 // kbotCmd represents the kbot command
 var kbotCmd = &cobra.Command{
@@ -81,27 +82,54 @@ This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		logger := zerodriver.NewProductionLogger()
-		fmt.Println("kbot started", AppVersion)
+
 		kbot, err := telebot.NewBot(telebot.Settings{
 			URL:    "",
-			Token:  token,
+			Token:  TeleToken,
 			Poller: &telebot.LongPoller{Timeout: 10 * time.Second},
 		})
 
 		if err != nil {
-			log.Fatalf("Please check TELE_TOKEN env variable. %s", err)
+			logger.Fatal().Str("Error", err.Error()).Msg("Please check TELE_TOKEN")
 			return
+		} else {
+			logger.Info().Str("Version", appVersion).Msg("kbot started")
+
 		}
 
+		trafficSignal := make(map[string]map[string]int8)
+
+		trafficSignal["red"] = make(map[string]int8)
+		trafficSignal["amber"] = make(map[string]int8)
+		trafficSignal["green"] = make(map[string]int8)
+
+		trafficSignal["red"]["pin"] = 12
+		trafficSignal["amber"]["pin"] = 27
+		trafficSignal["green"]["pin"] = 22
+
 		kbot.Handle(telebot.OnText, func(m telebot.Context) error {
-			log.Print(m.Message().Payload, m.Text())
+			logger.Info().Str("Payload", m.Text()).Msg(m.Message().Payload)
+
 			payload := m.Message().Payload
+			pmetrics(context.Background(), payload)
 
 			switch payload {
 			case "hello":
-				err = m.Send(fmt.Sprintf("Hello I'm Kbot %s!", AppVersion))
-			case "goodbye":
-				err = m.Send(fmt.Sprintf("Farewell!(^_^) %s!", AppVersion))
+				err = m.Send(fmt.Sprintf("Hello I'm Kbot %s!", appVersion))
+
+			case "red", "amber", "green":
+
+				if trafficSignal[payload]["on"] == 0 {
+					trafficSignal[payload]["on"] = 1
+				} else {
+					trafficSignal[payload]["on"] = 0
+				}
+
+				err = m.Send(fmt.Sprintf("Switch %s light signal to %d", payload, trafficSignal[payload]["on"]))
+
+			default:
+				err = m.Send("Usage: /s red|amber|green")
+
 			}
 
 			return err
@@ -126,4 +154,7 @@ func init() {
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
 	// kbotCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+
+	// Initialize OpenTelemetry tracer
+
 }
